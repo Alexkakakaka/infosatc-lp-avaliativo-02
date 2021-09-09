76e3df412829a5908d55446bdc13ad1c677abd2d
Alex Rodrigues <kakaloko4545@outlook.com>
2021-09-09 16:45:11 -0300

    Create python5.py
    numero07

---
+++ python5.py
@@ -0,0 +1,48 @@
+
+lista1 = ["filme"]
+print(lista1)
+
+lista2 = ["jogos"]
+print(lista2)
+
+lista3 = ["livros"]
+print(lista3)
+
+lista4 = ["esporte"]
+print(lista4)
+
+#Letra A
+lista1.append("Impativel")
+lista1.append("JogosMortais")
+print(lista1)
+
+lista2.append("Mortalkombat")
+lista2.append("GodofWar")
+print(lista2)
+
+lista3.append("Greciaantiga")
+lista3.append("Harrypotter")
+print(lista3)
+
+lista4.append("Futebol")
+lista4.append("Golf")
+print(lista4)
+
+#Letra B
+nova_lista = ["filme","jogos","livros","esporte"]
+nova_lista.append
+print("Minha nova lista",nova_lista)
+
+#Letra C
+print(lista3.index('Harrypotter'))
+
+#Letra D
+del lista4[2]
+print(lista4)
+
+#Letra E
+nova_lista.append("Disciplina")
+print(nova_lista)
+
+
+
